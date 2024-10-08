51c7caf736ad7626e90860be97498be9612c3ce8
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-10-09 01:58:32 +0530

    add loops

---
+++ Loops/src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+package com.devmare
+
+fun main() {
+
+//    for (i in 1..10) {
+//        println(i)
+//    }
+
+
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+
+
+//    for (i in 10 downTo 1) {
+//        println(i)
+//    }
+
+
+//    for (i in 1 until 10 step 5) {
+//        println(i)
+//    }
+
+    var i = 0
+//    while(i-- > 0) {
+//        println("hello")
+//    }
+
+//    outer@ while (i < 5) {
+//        i++
+//        println(i)
+//        var j = 0
+//        while (j < 3) {
+//            if (i == 2) {
+//                break@outer
+//            }
+//            j++
+//            println("$i - $j")
+//        }
+//    }
+    println(i)
+}
