506861f9667d3a07a8c7f75cf297d6c8b91be170
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-10-08 19:56:44 +0530

    learning kotlin

---
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.devmare
+
+fun main() {
+    println("Hello World!")
+}
---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package com.devmare
+
+fun main() {
+
+    var name: String = "Ankush"
+    name = "Paul"
+
+    val age: Int = 22
+    // age = 24  // 'val' cannot be reassigned.
+
+    println("Hello $name, $age")
+}
